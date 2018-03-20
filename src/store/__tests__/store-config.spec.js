dd115f36c271c8a29037b8cb5c8f4c34f1e4b5af
eddyerburgh <edward.yerburgh@gmail.com>
2018-03-20 07:20:49 +0000

    fix: remove counter test

--- src/store/__tests__/store-config.spec.js
+++ src/store/__tests__/store-config.spec.js
@@ -31,21 +31,4 @@ describe('store-config', () => {
     expect(store.getters.displayItems).toHaveLength(20)
     expect(store.getters.displayItems.every((item, i) => item === items[i])).toBe(true)// #I
   })
-
-  test('increment updates state.count by 1', async () => {
-    Vue.use(Vuex)
-    const clonedStoreConfig = deepClone(storeConfig)
-    const store = new Vuex.Store(clonedStoreConfig)
-    expect(store.state.count).toBe(0)
-    store.commit('increment')
-    expect(store.state.count).toBe(1)
-  })
-
-  test.only('calling fetchListData with the type returns top 20 activeItems from activeItems getter', async () => {
-    Vue.use(Vuex)
-    const clonedStoreConfig = deepClone(storeConfig)
-    const store = new Vuex.Store(clonedStoreConfig)
-    store.commit('increment')
-    expect(store.state.count).toBe(1)
-  })
 })
